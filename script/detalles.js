38394b0cd7484b1dad1a98532ab1cf8d2f056b12
Ignacio SuarezB <suarezb.ignacio@gmail.com>
2021-11-09 11:23:27 -0300

    terminando detalles, funciones eliminar

--- script/detalles.js
+++ script/detalles.js
@@ -57,6 +57,46 @@ function cargarDatos() {
     //actualizarGeneral();
   }
 }
+function eliminarGasto(indexPersona, indexGasto) {
+  //console.log("anda");
+  const longPropio = personas[indexPersona].gastosPropios.length;
+  if (longPropio > indexGasto) {
+    //Gastos Propio
+    personas[indexPersona].gastopropio -=
+      personas[indexPersona].gastosPropios[indexGasto].gasto;
+    personas[indexPersona].gastototal -=
+      personas[indexPersona].gastosPropios[indexGasto].gasto;
+    //console.log("Gastos Propio");
+    //console.log(personas[indexPersona].gastopropio);
+    personas[indexPersona].gastosPropios.splice(indexGasto, 1);
+  } else if (longPropio <= indexGasto) {
+    //Gastos Grupales
+    personas[indexPersona].gastogeneral -=
+      personas[indexPersona].gastosGeneralesPagados[
+        indexGasto - longPropio
+      ].gasto;
+
+    personas[indexPersona].gastototal -=
+      personas[indexPersona].gastosGeneralesPagados[
+        indexGasto - longPropio
+      ].gasto;
+
+    gastosgeneralesglobal -=
+      personas[indexPersona].gastosGeneralesPagados[indexGasto - longPropio]
+        .gasto;
+    //console.log("Gastos General");
+    //console.log(personas[indexPersona].gastogeneral);
+    personas[indexPersona].gastosGeneralesPagados.splice(
+      indexGasto - longPropio,
+      1
+    );
+  }
+  cargarDetalles(indexPersona);
+  calcularDevolver();
+  localStorage.setItem("storagePersonas", JSON.stringify(personas));
+  localStorage.setItem("storageGlobal", gastosgeneralesglobal);
+}
+
 function cargarDetalles(seleccionado) {
   $("#tabla-gastos").empty();
   const todosLosGastos = personas[seleccionado].gastosPropios.concat(
@@ -73,10 +113,41 @@ function cargarDetalles(seleccionado) {
     fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear()
   }</td>
   <td>${gasto.descripcion}</td>
+  <td > <button type="button" class="btn btn-danger eliminar" value="${index}">X</button></td>
 </tr>`);
   });
 }
+function calcularDevolver() {
+  personas.forEach((cada) => {
+    cada.devolver = gastosgeneralesglobal / personas.length - cada.gastogeneral;
+  });
+}
+
 $(document).ready(cargarDatos());
+
 $("#person-select").on("change", function () {
   cargarDetalles($("#person-select").val());
+});
+
+$("#tabla-gastos").click(function (e) {
+  console.log(e.target.classList.contains("eliminar"));
+  if (e.target.classList.contains("eliminar")) {
+    console.log($("#person-select").val(), e.target.value);
+    eliminarGasto($("#person-select").val(), e.target.value);
+  }
+  e.stopPropagation();
+});
+
+$("#eliminar-persona").click(function (e) {
+  personas.splice($("#person-select").val(), 1);
+  gastosgeneralesglobal = 0;
+  personas.forEach((persona) => {
+    gastosgeneralesglobal += persona.gastogeneral;
+  });
+  console.log(gastosgeneralesglobal);
+  calcularDevolver();
+  localStorage.setItem("storagePersonas", JSON.stringify(personas));
+  localStorage.setItem("storageGlobal", gastosgeneralesglobal);
+  location.reload();
+  e.stopPropagation();
 });
