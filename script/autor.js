52dfbd369d1f2b978ae9f8a765972284b3bd17c0
Ignacio SuarezB <suarezb.ignacio@gmail.com>
2021-11-15 10:50:18 -0300

    efect jquery

--- script/autor.js
+++ script/autor.js
@@ -1,4 +1,13 @@
 $("#restart").click(function (e) {
   localStorage.clear();
   e.stopPropagation();
+  $(".trash-svg")
+    .fadeOut("slow")
+    .css({ color: "red", opacity: "1" })
+    .fadeIn("slow")
+    .fadeOut("slow")
+    .fadeIn("slow")
+    .animate({ opacity: "0.6" }, "slow");
 });
+
+$(".trash-svg").hide();
